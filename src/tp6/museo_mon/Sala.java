b14dfe8b1a89d06995ea26b37f2909154a243c1d
Franco Hielpos <fhielpos@gmail.com>
2020-11-12 18:46:12 -0300

    Museo con monitores

---
+++ src/tp6/museo_mon/Sala.java
@@ -0,0 +1,73 @@
+package tp6.museo_mon;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Sala {
+    private int capacidadSala;
+    private int capacidadMaxima;
+    private int personasDentro;
+    private int temperatura;
+    private int capacidadReducida = 35;
+
+    public Sala(int limiteSala) {
+        this.personasDentro = 0;
+        this.capacidadSala = limiteSala;
+        this.capacidadMaxima = limiteSala;
+    }
+
+    public synchronized void restringirCapacidad() {
+        this.capacidadSala = this.capacidadReducida;
+    }
+
+    public synchronized void restaurarCapacidad() {
+        this.capacidadSala = this.capacidadMaxima;
+    }
+
+    public synchronized void entrarSala() {
+        try {
+            while (personasDentro >= capacidadSala) {
+                System.out.println("Sala llena esperando");
+                this.wait();
+            }
+            this.personasDentro++;
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    public synchronized void entrarSalaJubilado() {
+        try {
+            while (personasDentro >= capacidadSala) {
+                System.out.println("Sala llena jubilado esperando");
+                this.wait();
+            }
+            this.personasDentro++;
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public synchronized void salirSala() {
+        this.personasDentro--;
+//        if (this.lockSala.hasWaiters(jubilado)) {
+//           jubilado.signalAll();
+//        } else {
+//            this.notifyAll();
+//        }
+    }
+
+    public synchronized int getCapacidad(){
+        return this.capacidadSala;
+    }
+
+    public synchronized int getTemperatura() {
+        return this.temperatura;
+    }
+
+    public synchronized void setTemperatura(int nuevaTemp){
+        this.temperatura = nuevaTemp;
+    }
+
+}
---
+++ src/tp6/museo_mon/Sensor.java
@@ -0,0 +1,33 @@
+package tp6.museo_mon;
+
+import java.util.Random;
+
+public class Sensor implements Runnable {
+    private int tUmbral;
+    private Sala museo;
+
+    public Sensor(Sala unMuseo, int umbral) {
+        this.museo = unMuseo;
+        this.tUmbral = umbral;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            this.museo.setTemperatura(new Random().nextInt(20) + 25);
+            if (this.museo.getTemperatura() >= tUmbral) {
+                System.out.println("SENSOR: Temperatura maxima, reduciendo");
+                museo.restringirCapacidad();
+            } else {
+                System.out.println("SENSOR: Temperatura baja, aumentando");
+                museo.restaurarCapacidad();
+            }
+            try {
+                System.out.println("CAPACIDAD: " +museo.getCapacidad());
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
