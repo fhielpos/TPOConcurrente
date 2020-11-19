9f6e0aca83c9774162fc9a8b2c50b4eb793536d6
E-Salman <71310857+E-Salman@users.noreply.github.com>
2020-11-19 18:15:53 -0300

    copiado

--- src/tp6/centro/CentroHemoterapia.java
+++ src/tp6/centro/CentroHemoterapia.java
@@ -7,8 +7,8 @@ import java.util.concurrent.locks.ReentrantLock;
 public class CentroHemoterapia {
     private ReentrantLock salaEspera = new ReentrantLock(true);
 
-    private Condition camillas = salaEspera.newCondition();
-    private Condition revistas = salaEspera.newCondition();
+    private Condition condCamillas = salaEspera.newCondition();
+    private Condition condRevistas = salaEspera.newCondition();
 
     private int cantRevistas;
     private int revistasDisponibles;
@@ -19,24 +19,38 @@ public class CentroHemoterapia {
     private int cantSillas;
     private int sillasDisponibles;
 
-    public CentroHemoterapia(int sillas, int camillas, int revistas){
-        this.cantCamillas = sillas;
+    private int turno;      // Turno para saber a que paciente atender
+    private int pacientes;  // Cuenta cuantos pacientes pasaron en el dia
+
+    public CentroHemoterapia(int limiteSillas, int limiteCamillas, int limiteRevistas) {
+        this.cantCamillas = limiteSillas;
         this.sillasDisponibles = this.cantSillas;
 
-        this.cantCamillas = camillas;
+        this.cantCamillas = limiteCamillas;
         this.camillasDisponibles = this.cantCamillas;
 
-        this.cantRevistas = revistas;
+        this.cantRevistas = limiteRevistas;
         this.revistasDisponibles = this.cantRevistas;
+
+        this.turno = this.camillasDisponibles;
+        this.pacientes = 0;
     }
 
-    public void entrarSala(){
+    public void entrarSala() {
         boolean poseeRevista = false;
         boolean estaSentado = false;
+        int miTurno;
 
-        try{
+        try {
             salaEspera.lock();
-            while(camillasDisponibles == 0){
+
+            this.pacientes++;
+            miTurno = this.pacientes;
+
+            // Mientras no sea mi turno o no haya camillas
+            // El turno < miTurno es por si sale mas de un donante a la vez, para que no se pase su turno
+            while (this.turno < miTurno || camillasDisponibles == 0) {
+
                 // Ver si hay sillas disponibles
                 if (this.sillasDisponibles != 0) {
                     this.sillasDisponibles--;
@@ -47,28 +61,34 @@ public class CentroHemoterapia {
                 if (!poseeRevista) {
                     while (this.revistasDisponibles == 0) {
                         // Ver la tele y esperar por una revista
-                        this.revistas.await(1000, TimeUnit.MILLISECONDS);
+                        this.condRevistas.await(1000, TimeUnit.MILLISECONDS);
                     }
                     this.revistasDisponibles--;
                     poseeRevista = true;
                 }
                 // Fin condicion revistas
+
+
                 if (estaSentado && poseeRevista)
                     // Posee todos los recursos, nomas espera a que se libere una camilla
-                    this.camillas.await();
+                    this.condCamillas.await();
                 else
                     // Podria sentarse o buscar una revista, esperar un rato y volver a intentar
-                    this.camillas.await(1000, TimeUnit.MILLISECONDS);
+                    this.condCamillas.await(1000, TimeUnit.MILLISECONDS);
             }
 
             // Hay camillas disponibles, liberar recursos
-            if(estaSentado)
+            if (estaSentado)
                 this.sillasDisponibles++;
-            if(poseeRevista){
+            if (poseeRevista) {
                 this.revistasDisponibles++;
-                this.revistas.signalAll();
+                this.condRevistas.signalAll();
             }
-        } catch (InterruptedException ex){
+
+            System.out.println("Thread entrando a donar: " + Thread.currentThread().getName() + " TURNO: " + this.turno + " PACIENTE: " + miTurno);
+            this.camillasDisponibles--;
+
+        } catch (InterruptedException ex) {
             ex.printStackTrace();
         } finally {
             salaEspera.unlock();
@@ -76,10 +96,12 @@ public class CentroHemoterapia {
 
     }
 
-    public void salir(){
+    public void salir() {
         this.salaEspera.lock();
         this.camillasDisponibles++;
-        this.camillas.signal();
+        this.turno++;
+        System.out.println(Thread.currentThread().getName() + " saliendo" + " TURNO: " + this.turno);
+        this.condCamillas.signalAll();
         this.salaEspera.unlock();
     }
 
