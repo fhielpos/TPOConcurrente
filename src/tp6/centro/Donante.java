efffe8da7823e5c94c73de9cfcda11e8c3345377
E-Salman <71310857+E-Salman@users.noreply.github.com>
2020-11-19 18:05:29 -0300

    copiado donante/main

---
+++ src/tp6/centro/Donante.java
@@ -0,0 +1,28 @@
+package tp6.centro;
+
+import java.util.Random;
+
+public class Donante implements Runnable {
+    private CentroHemoterapia centro;
+
+    public Donante(CentroHemoterapia unCentro) {
+        this.centro = unCentro;
+    }
+
+    @Override
+    public void run() {
+        // Yendo a donar
+        centro.entrarSala();
+        // Donando
+        try {
+            System.out.println(Thread.currentThread().getName() + " donando...");
+            Thread.sleep(new Random().nextInt(4000) + 1500);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        // Saliendo del centro
+        System.out.println(Thread.currentThread().getName() + " saliendo");
+        centro.salir();
+    }
+}
