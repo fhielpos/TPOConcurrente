9d7e5b9a38c7478cf2c86b90fef1ede9ce8c01bf
Franco Hielpos <fhielpos@gmail.com>
2020-11-19 17:25:51 -0300

    Comentarios y cambios en los locks

--- src/tp6/centro/CentroHemoterapia.java
+++ src/tp6/centro/CentroHemoterapia.java
@@ -1,11 +1,11 @@
 package tp6.centro;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CentroHemoterapia {
-    private ReentrantLock salaEspera = new ReentrantLock();
-    private ReentrantLock lockCamillas = new ReentrantLock(true);
+    private ReentrantLock salaEspera = new ReentrantLock(true);
 
     private Condition camillas = salaEspera.newCondition();
     private Condition revistas = salaEspera.newCondition();
@@ -47,34 +47,40 @@ public class CentroHemoterapia {
                 if (!poseeRevista) {
                     while (this.revistasDisponibles == 0) {
                         // Ver la tele y esperar por una revista
-                        this.revistas.await();
+                        this.revistas.await(1000, TimeUnit.MILLISECONDS);
                     }
                     this.revistasDisponibles--;
                     poseeRevista = true;
                 }
-
                 // Fin condicion revistas
+                if (estaSentado && poseeRevista)
+                    // Posee todos los recursos, nomas espera a que se libere una camilla
+                    this.camillas.await();
+                else
+                    // Podria sentarse o buscar una revista, esperar un rato y volver a intentar
+                    this.camillas.await(1000, TimeUnit.MILLISECONDS);
             }
 
-            // Hay camas disponibles, liberar recursos
+            // Hay camillas disponibles, liberar recursos
             if(estaSentado)
                 this.sillasDisponibles++;
             if(poseeRevista){
                 this.revistasDisponibles++;
-                this.revistas.notifyAll();
+                this.revistas.signalAll();
             }
-
         } catch (InterruptedException ex){
             ex.printStackTrace();
         } finally {
             salaEspera.unlock();
         }
+
     }
 
-    public void donar(){
-        this.lockCamillas.lock();
-
-        this.lockCamillas.unlock();
+    public void salir(){
+        this.salaEspera.lock();
+        this.camillasDisponibles++;
+        this.camillas.signal();
+        this.salaEspera.unlock();
     }
 
 }
